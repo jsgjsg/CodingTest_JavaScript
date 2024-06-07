b8dbdb16457d294b8d46a166af8d3062dd7d1549
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-06-07 22:49:26 +0900

    [level 1] Title: 기사단원의 무기, Time: 19.12 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,22 +1,33 @@
 function solution(number, limit, power) {
     let answer = 0;
     
+    let prime = getPrime(number);
     for(let i = 1; i <= number; i++) {
-        let count = 0;
-        for(let j = 1; j <= Math.sqrt(i); j++) {
-            if(i % j == 0) {
-                if(i == j * j) count++;
-                else count += 2;
+        let cur = i;
+        let count = 1;
+        for(let j = 2; j <= i; j++) {
+            if(!prime[j]) continue;
+            let tmp = 1;
+            while(cur % j == 0) {
+                tmp++;
+                cur = ~~(cur / j);
             }
-            
-            if(count > limit) {
-                count = power;
-                break;
-            }
+            count *= tmp;
+            if(count > limit) break;
         }
-        console.log(count);
-        answer += count;
+        answer += count > limit ? power : count;
     }
     
     return answer;
+}
+function getPrime(n) {
+    let prime = new Array(n + 1).fill(true);
+    prime[0] = prime[1] = false;
+    
+    for(let i = 2; i < Math.sqrt(n); i++) {
+        if(prime[i]) {
+            for(let j = i * 2; j <= n; j += i) prime[j] = false;
+        }
+    }
+    return prime;
 }
