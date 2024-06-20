2c2799aecaf782c010a7ffccb0f1874eb86d1471
전승규 <110209803+jsgjsg@users.noreply.github.com>
2024-06-20 23:46:41 +0900

    [level 1] Title: 덧칠하기, Time: 1.47 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,13 +1,4 @@
 function solution(n, m, section) {
-    let answer = 0;
-    
-    let fin = 0;
-    section.forEach((item) => {
-        if(item <= fin) return;
-        
-        fin = item + m - 1;
-        answer++;
-    })
-    
+    var answer = 0;
     return answer;
 }
